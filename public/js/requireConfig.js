d44f5a706dbaf4d5628fa36cf89450765aea1eb6
jaehwan <woghks1119@naver.com>
2015-01-21 23:04:51 +0900

    angular-material setting

--- public/js/requireConfig.js
+++ public/js/requireConfig.js
@@ -1,7 +1,7 @@
 /**
  * Created by LEE_fA on 2015-01-18.
  */
-requirejs.config({
+require.config({
     'baseUrl' : 'js',
 
     'paths' : {
@@ -9,14 +9,30 @@ requirejs.config({
         'async' : '../../bower_components/async/lib/async',
         'angular' : '../../bower_components/angular/angular',
         'angular-route' : '../../bower_components/angular-route/angular-route',
-        'angularAMD' : '../../bower_components/angularAMD/angularAMD'
+        'angularAMD' : '../../bower_components/angularAMD/angularAMD',
+        'angular-material' : '../../bower_components/angular-material/angular-material',
+        'angular-animate' : '../../bower_components/angular-animate/angular-animate',
+        'angular-aria' : '../../bower_components/angular-aria/angular-aria',
+        'angular-resource' : '../../bower_components/angular-resource/angular-resource',
+        'hammer' : '../../bower_components/hammerjs/hammer'
     },
     'shim' : {
         'angular-route' : ['angular'],
-        'angularAMD' : ['angular']
+        'angularAMD' : ['angular'],
+        'angular-animate' : ['angular'],
+        'angular-aria' : ['angular'],
+        'angular-resource' : ['angular'],
+        'angular-material' : [
+            'angular-animate',
+            'angular-aria',
+            'hammer',
+            'css!../../bower_components/angular-material/angular-material.min',
+            'css!../../bower_components/angular-material/theme/grey-theme'
+        ]
     },
     'map' : {
         '*' : {
+            'css' : '../../bower_components/require-css/css',
             'Controller' : '../controllers',
             'Service' : '../services',
             'Filter' : '../filters',
